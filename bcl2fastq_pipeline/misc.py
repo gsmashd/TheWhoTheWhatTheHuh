df4c74c194c16e9f23a7e3093a775a1355ee8edd
Geir A <geiramh@stud.ntnu.no>
2019-01-31 13:32:50 +0100

    write finished reports to disk

--- bcl2fastq_pipeline/misc.py
+++ bcl2fastq_pipeline/misc.py
@@ -131,13 +131,18 @@ def enoughFreeSpace(config) :
 
 def errorEmail(config, errTuple, msg) :
     msg = MIMEText(msg + "\nError type: %s\nError value: %s\n%s\n" % (errTuple[0], errTuple[1], errTuple[2]))
+    """
     msg['Subject'] = "[bcl2fastq_pipeline] Error"
     msg['From'] = config.get("Email","fromAddress")
     msg['To'] = config.get("Email","errorTo")
-
+    """
+    with open(os.path.join(config.get("Paths", "outputDir"),'{}.report'.format(config.get("Options","runID"))),'w') as report:
+        report.write(msg)
+    """
     s = smtplib.SMTP(config.get("Email","host"))
     s.send_message(msg)
     s.quit()
+    """
 
 def finishedEmail(config, msg, runTime, transferTime) :
     lanes = config.get("Options", "lanes")
@@ -149,6 +154,9 @@ def finishedEmail(config, msg, runTime, transferTime) :
     message += "Data transfer: %s\n" % transferTime
     message += msg
 
+    with open(os.path.join(config.get("Paths", "outputDir"),'{}.report'.format(config.get("Options","runID"))),'w') as report:
+        report.write(msg)
+    """
     msg = MIMEText(message)
     msg['Subject'] = "[bcl2fastq_pipeline] %s%s processed" % (config.get("Options","runID"), lanes)
     msg['From'] = config.get("Email","fromAddress")
@@ -157,4 +165,4 @@ def finishedEmail(config, msg, runTime, transferTime) :
     s = smtplib.SMTP(config.get("Email","host"))
     s.send_message(msg)
     s.quit()
-
+    """
